93774afc2011bcf52e603c3bab8f8ef29c5ef7bf
cybersam07 <hackspycyber@gmail.com>
2025-01-30 21:47:54 +0100

    Fixed Cannot GET / error

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express');
 const nodemailer = require('nodemailer');
-const cors = require('cors');
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cors());
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
